cce95c64fee6e57d43fa8dfa8e0e4e1c8f47d405
DESKTOP-FV3AR0O\levis <leviskim17@gmail.com>
2017-06-28 18:32:39 +0900

    Add generateType2

--- XMLToLinear/src/nus/iss/cbr/cocktail/xml2linear/xml/XMLParser.java
+++ XMLToLinear/src/nus/iss/cbr/cocktail/xml2linear/xml/XMLParser.java
@@ -58,7 +58,7 @@ public class XMLParser {
 		}
 	}
 	
-	public void generate(String xmlFilePath, String txtFilePath) {
+	public void generateType1(String xmlFilePath, String txtFilePath) {
 		try {
 			File xmlFile = new File(xmlFilePath);
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -73,20 +73,20 @@ public class XMLParser {
 			out.write(buffer); 
 			out.newLine();
 			
-			buffer += "#caseId;title,food,unit,quantity,step";
+			buffer += "#caseId:title:food(repetitive):unit(repetitive):quantity(repetitive):ingredient(repetitive):step(repetitive)";
 			out.write(buffer); 
 			out.newLine();
 			out.newLine();
 
 			NodeList recipeLst = doc.getElementsByTagName("recipe");
 			for (int recipeIdx = 0; recipeIdx < recipeLst.getLength(); recipeIdx++) {
-				buffer = "case" + (recipeIdx + 1) + ";";
+				buffer = "case" + (recipeIdx + 1) + ":";
 				
 				Node recipeNode = recipeLst.item(recipeIdx);
 				if (recipeNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element recipeElmt = (Element) recipeNode;
-					System.out.println("title : " + getElementValue("title", recipeElmt));
-					buffer += getElementValue("title", recipeElmt) + ";";
+					//System.out.println("title : " + getElementValue("title", recipeElmt));
+					buffer += getElementValue("title", recipeElmt) + ":";
 					
 					NodeList ingrediantLst = recipeElmt.getElementsByTagName("ingredients").item(0).getChildNodes();
 					for (int ingrediantIdx = 0; ingrediantIdx < ingrediantLst.getLength(); ingrediantIdx++) {
@@ -94,15 +94,15 @@ public class XMLParser {
 						if (ingrediantNode.getNodeType() == Node.ELEMENT_NODE) {
 							Element ingrediantElmt = (Element)ingrediantNode;
 							
-							buffer += getAttributeValue("food", ingrediantElmt) + ";";
-							buffer += getAttributeValue("unit", ingrediantElmt) + ";";
-							buffer += getAttributeValue("quantity", ingrediantElmt) + ";";
-							buffer += getElementValue(ingrediantElmt) + ";";
+							buffer += getAttributeValue("food", ingrediantElmt) + ":";
+							buffer += getAttributeValue("unit", ingrediantElmt) + ":";
+							buffer += getAttributeValue("quantity", ingrediantElmt) + ":";
+							buffer += getElementValue(ingrediantElmt) + ":";
 							
-							System.out.println("	food : " + getAttributeValue("food", ingrediantElmt));
-							System.out.println("	unit : " + getAttributeValue("unit", ingrediantElmt));
-							System.out.println("	quantity : " + getAttributeValue("quantity", ingrediantElmt));
-							System.out.println("	ingredient : " + getElementValue(ingrediantElmt));
+							//System.out.println("	food : " + getAttributeValue("food", ingrediantElmt));
+							//System.out.println("	unit : " + getAttributeValue("unit", ingrediantElmt));
+							//System.out.println("	quantity : " + getAttributeValue("quantity", ingrediantElmt));
+							//System.out.println("	ingredient : " + getElementValue(ingrediantElmt));
 						}
 					}
 					
@@ -112,10 +112,77 @@ public class XMLParser {
 						if (stepNode.getNodeType() == Node.ELEMENT_NODE) {
 							Element stepElmt = (Element)stepNode;
 							
-							buffer += getElementValue(stepElmt) + ";";
-							System.out.println("	step : " + getElementValue(stepElmt));
+							buffer += getElementValue(stepElmt) + ":";
+							//System.out.println("	step : " + getElementValue(stepElmt));
 						}
 					}
+				}
+				
+				out.write(buffer); 
+				out.newLine();
+			}
+			
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void generateType2(String xmlFilePath, String txtFilePath) {
+		try {
+			File xmlFile = new File(xmlFilePath);
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(xmlFile);
+			doc.getDocumentElement().normalize();
+			
+			BufferedWriter out = new BufferedWriter(new FileWriter(txtFilePath));
+			String buffer = "";
+			
+			buffer += "#CocktailPlainTextCaseBase";
+			out.write(buffer); 
+			out.newLine();
+			
+			buffer += "#caseId;title,ingredient(united),step(united)";
+			out.write(buffer); 
+			out.newLine();
+			out.newLine();
+
+			NodeList recipeLst = doc.getElementsByTagName("recipe");
+			for (int recipeIdx = 0; recipeIdx < recipeLst.getLength(); recipeIdx++) {
+				buffer = "case" + (recipeIdx + 1) + ":";
+				
+				Node recipeNode = recipeLst.item(recipeIdx);
+				if (recipeNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element recipeElmt = (Element) recipeNode;
+					//System.out.println("title : " + getElementValue("title", recipeElmt));
+					buffer += "/title:" + getElementValue("title", recipeElmt);
+					
+					NodeList ingrediantLst = recipeElmt.getElementsByTagName("ingredients").item(0).getChildNodes();
+					for (int ingrediantIdx = 0; ingrediantIdx < ingrediantLst.getLength(); ingrediantIdx++) {
+						Node ingrediantNode = ingrediantLst.item(ingrediantIdx);
+						if (ingrediantNode.getNodeType() == Node.ELEMENT_NODE) {
+							Element ingrediantElmt = (Element)ingrediantNode;
+							
+							buffer += "/food:" + getAttributeValue("food", ingrediantElmt);
+							buffer += "/quantity:" + getAttributeValue("quantity", ingrediantElmt);
+							buffer += "/unit:" + getAttributeValue("unit", ingrediantElmt);
+						}
+					}
+					
+					buffer += "/step:";
+					
+					NodeList stepLst = recipeElmt.getElementsByTagName("preparation").item(0).getChildNodes();
+					for (int stepIdx = 0; stepIdx < stepLst.getLength(); stepIdx++) {
+						Node stepNode = stepLst.item(stepIdx);
+						if (stepNode.getNodeType() == Node.ELEMENT_NODE) {
+							Element stepElmt = (Element)stepNode;
+							
+							buffer += getElementValue(stepElmt) + " ";
+						}
+					}
+					
+					buffer += "/";
 				}
 				
 				out.write(buffer); 
