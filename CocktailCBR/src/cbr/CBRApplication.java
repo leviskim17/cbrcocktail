0e3e5806b36c4bac1e3d065b2e49d1f2a30b7620
mateng0915 <mateng915@outlook.com>
2017-06-30 05:35:46 +0800

    4

---
+++ CocktailCBR/src/cbr/CBRApplication.java
@@ -0,0 +1,115 @@
+package cbr;
+
+import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.CBRCase;
+import jcolibri.cbrcore.CBRCaseBase;
+import jcolibri.cbrcore.CBRQuery;
+import jcolibri.cbrcore.Connector;
+import jcolibri.exception.ExecutionException;
+import jcolibri.exception.InitializingException;
+import jcolibri.method.retrieve.NNretrieval.NNConfig;
+
+import javax.annotation.Generated;
+import representation.CaseDescription;
+import jcolibri.method.retrieve.NNretrieval.NNConfig;
+import jcolibri.cbrcore.Attribute;
+
+public class CBRApplication implements StandardCBRApplication {
+
+	@Generated(value = { "ColibriStudio" })
+	Connector connector;
+	
+	@Generated(value = { "ColibriStudio" })
+	CBRCaseBase casebase;
+
+
+
+	//******************************************************************/
+	// Configuration
+	//******************************************************************/
+
+	@Override
+	public void configure() throws ExecutionException {
+		try{
+			configureConnector();
+			configureCaseBase();
+		} catch (Exception e){
+			throw new ExecutionException(e);
+		}
+	}
+
+	/** Configures the connector */
+	@Generated(value = { "CS-PTConector" })	
+	private void configureConnector() throws InitializingException{
+		connector = new jcolibri.connector.PlainTextConnector();
+		connector.initFromXMLfile(jcolibri.util.FileIO
+				.findFile("config/plainTextConnectorConfig.xml"));
+	}
+
+	/** Configures the case base */
+	@Generated(value = { "CS-CaseManager" })	
+	private void configureCaseBase() throws InitializingException{
+		casebase = new jcolibri.casebase.LinearCaseBase();
+	}
+
+	//******************************************************************/
+	// Similarity
+	//******************************************************************/
+	
+	/** Configures the similarity */
+	@Generated(value = { "CS-Similarity" })	
+	private NNConfig getSimilarityConfig() {
+		NNConfig simConfig = new NNConfig();
+		simConfig
+				.setDescriptionSimFunction(new jcolibri.method.retrieve.NNretrieval.similarity.global.Average());
+		Attribute attribute0 = new Attribute("CaseId", CaseDescription.class);
+		simConfig
+				.addMapping(
+						attribute0,
+						new jcolibri.method.retrieve.NNretrieval.similarity.local.Interval());
+		simConfig.setWeight(attribute0, 1.00);
+		return simConfig;
+	}
+
+	//******************************************************************/
+	// Methods
+	//******************************************************************/
+	
+
+	@Generated(value = { "ColibriStudio" })
+	@Override
+	public CBRCaseBase preCycle() throws ExecutionException {
+		casebase.init(connector);
+		return casebase;
+	}
+		
+	@Generated(value = { "ColibriStudio" })	
+	@Override
+	public void cycle(CBRQuery query) throws ExecutionException {
+	}
+
+	@Generated(value = { "ColibriStudio" })
+	@Override
+	public void postCycle() throws ExecutionException {
+		connector.close();
+	}
+
+	/**
+	 * Main method
+	 */
+	@Generated(value = { "ColibriStudio" })
+	public static void main(String[] args) {
+		CBRApplication cbrApp = new CBRApplication();
+		try {
+			cbrApp.configure();
+			CBRCaseBase caseBase = cbrApp.preCycle();
+			
+			for(CBRCase c: caseBase.getCases())
+				System.out.println(c);
+			
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
